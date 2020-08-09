d1d1ae65fe69bb75cf3bfe3aaeb2b83aae390e56
aliul azim <aliulazimbd@gmail.com>
2020-08-09 21:40:31 +0600

    data types

---
+++ 02_Data_Types/apps.js
@@ -0,0 +1,55 @@
+// Data Type in JavaScript
+
+//Primitives Data Types
+//Complex Data Types
+
+//PRIMITIVES DATA TYPES
+
+//Boolean
+// Numder
+// String
+// Null
+// Symbol (JavaScript new data types)
+// Undefined
+
+
+// COMPLEX DATA TYPES
+
+// Object
+
+
+
+// Boolean
+let val = true;
+console.log(typeof val);
+
+//Number
+let num = 25;
+console.log(typeof num);
+ 
+// String
+let text = "Hello Wrold";
+console.log(typeof text);
+
+//Null
+let sampleVar = null;
+console.log(typeof sampleVar); //Null javascript bug becouse null result shows Object
+
+// Symbol
+let symVal = Symbol();
+console.log(typeof symVal);
+
+//Undefined
+let unVal;
+console.log(typeof unVal);
+ 
+
+// COMPLEX DATA TYPES
+
+// Object
+let obj = {
+    name: "MD. ALIUL AZIM",
+    age: 25,
+
+}
+console.log(typeof obj);
