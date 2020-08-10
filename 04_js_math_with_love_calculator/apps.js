3db307466b7fa364a79f29d9b5c843e56a476ba2
aliul azim <aliulazimbd@gmail.com>
2020-08-10 23:10:09 +0600

    Love Calculator

---
+++ 04_js_math_with_love_calculator/apps.js
@@ -0,0 +1,59 @@
+// let value;
+// let num1 = 15;
+// let num2 = 10;
+
+
+// // Simple Math
+// value = num1 + num2;
+// value = num1 - num2;
+// value = num1 * num2;
+// value = num1 / num2;
+// value = num1 % num2;
+
+// // Object Math
+
+// value = Math.PI;
+// value = Math.E;
+// value = Math.round(25.7);
+// value = Math.ceil(2.14); // Result show next puno number
+// value = Math.floor(3.98); // Result show previuse puno number
+// value = Math.sqrt(16);
+// value = Math.pow(8, 2);
+// value = Math.abs(-5);
+// value = Math.min(14,12,4,55);
+// value = Math.max(14,12,4,55);
+
+// value = Math.random() * 100;
+// value = Math.floor(value) + 1;
+// // value = Math.floor(Math.random() * 100);
+
+
+
+
+// // Outpute
+
+// console.log(value);
+
+
+
+
+
+
+
+// Love Calculator
+
+//Persons Name
+let firstName = prompt("What is your name?");
+let partnerName = prompt("What is your partner name?");
+
+// Calculation
+let number = Math.random() * 100;
+number = Math.floor(number) + 1;
+
+
+// Showing Result
+alert(firstName + " and " + partnerName + "'s love percentage is: " + number + "%");
+
+
+// console.log(firstName);
+// console.log(partnerName);
