e4e3fd76ad6dce5bbb9f609afd1bc7ac724d56d5
aliul azim <aliulazimbd@gmail.com>
2020-08-21 09:43:59 +0600

    Nodes vs Elements in (DOM)

---
+++ 15_nodes_vs_elements_in_(DOM)/apps.js
@@ -0,0 +1,26 @@
+
+/*
+----------------------------------------------------
+                NODE TYPE
+----------------------------------------------------
+        ELEMENT_NODE                        : 1                
+        ATTRIBUTE_NODE                      : 2                
+        TEXT_NODE                           : 3                
+        CDATA_SECTION_NODE                  : 4                
+        ENTITY_REFERENCE_NODE               : 5                
+        ENTITY_NODE                         : 6                
+        PROCESSING_INSTRUCTION_NODE         : 7                
+        COMMENT_NODE                        : 8                
+        DOCUMENT_NODE                       : 9                
+        DOCUMENT_TYPE_NODE                  : 10                
+        DOCUMENT_FRAGMENT_NODE              : 11                
+        NOTATION_NODE                       : 12                
+
+
+----------------------------------------------------
+                DEPRICATED: 2, 5, 6, 12 
+----------------------------------------------------
+
+*/
+
+document.querySelector('body').childNodes[1].nodeType;
