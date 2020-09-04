cddc31b7c1fb0a5b4e7bf22407de9c13fd8b8efa
aliul azim <aliulazimbd@gmail.com>
2020-09-04 16:57:40 +0600

    Advanceed Data Types-Primitive Data Types VS Reference Data Types

---
+++ 30_advanced_data_type_primitive_data_types_vs_reference_data_type/apps.js
@@ -0,0 +1,23 @@
+// Primitive Data Types 
+
+let num = 14;
+let newNum = num;
+num = 50;
+
+console.log("The value of newNum is: ", newNum); //Primitive Data Types  new value assign korle age ono name je varible
+// create kore ager varible take assign kara hoycha seta value cheang habe na karon se memorite 1ta 8 bits size box niye nicha
+console.log("The value of num is: ", num);
+
+
+
+// Reference Dara Types
+
+let person = { name: "ALIUL"};
+let newPerson = person;
+person.name = "AZIM";
+
+console.log("The value of newPerson is: ", newPerson);
+console.log("The value of person is: ", person);
+
+// ******** Reference Dara Types memorite nidisto kono size nite pare na se address madome varible search kare 
+// tai value cheang korle sob jaigai value cheang hoye jai 
